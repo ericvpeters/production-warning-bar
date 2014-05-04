fafc0a0cfcb06390cd9697b91913609310e2fbf5
Eric Peters <epeters@yfc.net>
2014-05-03 21:42:07 -0600

    First commit of all the files

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 production-warning-bar
 ======================
 
-A chrome extension to overlay a warning bar when the use is on the productions site
+A chrome extension to overlay a warning bar when the use is on a productions site. This is to be a constant reminder to be careful with the data on the site.
+
+https://chrome.google.com/webstore/detail/jdgjpoeggllpkbdjpfipmcifjgjeblpe/publish-delayed?hl=en&gl=US
---
+++ options.js
@@ -0,0 +1,56 @@
+/**
+ * Save optional values to the chrome storage
+ */
+(function() {
+    //get current values with reasonable values
+    chrome.storage.sync.get({
+        barPosition: 'top',
+        domains: '',
+        barColor: 'FF0000',
+        barText: 'In Production Environment'
+    }, function(items) {
+        //autofill the values to the form elements
+        document.getElementById('barPosition').value = items.barPosition;
+        document.getElementById('domains').value = items.domains;
+        document.getElementById('barColor').value = items.barColor;
+        document.getElementById('barText').value = items.barText;
+        document.getElementById('barColor').style.backgroundColor = "#" + items.barColor;
+    });
+
+    document.getElementById('save').onclick = onSaveClick;
+
+    document.getElementById('barColor').onchange = onBarChange;
+
+    /**
+     * save the values to chrome storage
+     * @returns {undefined}
+     */
+    function onSaveClick() {
+        var barPosition = document.getElementById('barPosition').value;
+        var domains = document.getElementById('domains').value;
+        var barColor = document.getElementById('barColor').value;
+        var barText = document.getElementById('barText').value;
+        chrome.storage.sync.set({
+            barPosition: barPosition,
+            domains: domains,
+            barColor: barColor,
+            barText: barText
+        }, function() {
+            //close options tab
+            chrome.tabs.getCurrent(function(tab) {
+                chrome.tabs.remove(tab.id, function() {
+                });
+            });
+        });
+    }
+    
+    /**
+     * change the background color of the color select text box to reflect the hex value change
+     */
+    function onBarChange() {
+        var elem = document.getElementById('barColor');
+        var barColor = elem.value;
+        console.log(barColor);
+        elem.style.backgroundColor = "#" + barColor;
+    }
+})();
